fda5cc81131f1180d2b0b52c3824a471dd579902
bodenko <dan@bodenko.com>
2024-10-15 20:59:27 +0100

    Update home.nix

--- final/home.nix
+++ final/home.nix
@@ -1,33 +1,40 @@
-{ pkgs, inputs, ... }:
+{ config, pkgs, ... }:
 
-{ 
-  imports = [
-    inputs.impermanence.nixosModules.home-manager.impermanence
+{
+  home.stateVersion = "24.05";
+  home.packages = [
+    pkgs.htop
+    pkgs.fortune
+    pkgs.git
+    pkgs.git-crypt
+    pkgs.gnupg
+    pkgs.pass
   ];
 
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  programs.home-manager.enable = true;
+  
+  programs.git = {
+    enable = true;
+    userName = "user1";
+    userEmail = "user1@bodenko.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
 
-  home.persistence."/persist/home" = {
-    directories = [
-      "Downloads"
-      "Music"
-      "Pictures"
-      "Documents"
-      "Videos"
-      "VirtualBox VMs"
-      ".gnupg"
-      ".ssh"
-      ".nixops"
-      ".local/share/keyrings"
-      ".local/share/direnv"
-      {
-        directory = ".local/share/Steam";
-        method = "symlink";
-      }
-    ];
-    files = [
-      ".screenrc"
-    ];
-    allowOther = true;
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
   };
 }
